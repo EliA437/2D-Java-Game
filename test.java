c70a9d28ea07c43f2084791c702a7482019bf315
EliA437 <allenmwnc.eli@gmail.com>
2024-12-22 18:06:46 -0600

    Initial Commit

--- test.java
+++ test.java
@@ -1,12 +1,7 @@
 class test {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println(multiply(3,4));
-
-        /*Bob Sagot*/
+        
     }
 
-    public static int multiply(int num1, int num2) {
-        return num1 * num2;
-    }
 }
