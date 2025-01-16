c5dba6e10975371ee7f4a2d5900b79cdcbf27ae9
EliA437 <allenmwnc.eli@gmail.com>
2025-01-15 18:07:15 -0600

    fixed image loading

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2D-Java-Game
+
+Currently just a work in progress game. Working on setting up the background tiles.
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -31,14 +31,14 @@ public class Player extends Entity {
     public void getPlayerImage() {
         try {
             // Load each image using FileInputStream and absolute file paths
-            up1 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_up_1.png"));
-            up2 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_up_2.png"));
-            down1 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_down_1.png"));
-            down2 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_down_2.png"));
-            left1 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_left_1.png"));
-            left2 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_left_2.png"));
-            right1 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_right_1.png"));
-            right2 = ImageIO.read(new FileInputStream("C:\\Users\\allen\\OneDrive\\Desktop\\1CS\\Winter Break Softmore Year Projects\\2D-Java-Game\\src\\res\\player\\boy_right_2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/src/res/player/boy_right_2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
